ddc385d3e4d28386f5947f491fc45df260e18207
hermann90 <hermannchefouet@gmail.com>
2023-09-24 09:01:58 +0200

    Init Angular pipeline

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isAuthenticated : boolean=false;
+  roles : any;
+  username : any;
+  accessToken! : string;
+
+  constructor(private http:HttpClient) { }
+
+  public login(username : string, password : string){
+    let options = {
+      headers : new HttpHeaders().set("Content-Type", "application/x-www-form-urlencoded")
+    }
+    let params=new HttpParams().set("username",username).set("password",password);
+    
+    return this.http.post(environment.backendHost+"/auth/login", params, options)
+  }
+
+  loadProfile(data: any){
+    this.isAuthenticated=true;
+    this.accessToken = data['access-token'];
+    let decodedJwt : any= jwtDecode(this.accessToken);
+
+    this.username = decodedJwt.sub;
+    this.roles = decodedJwt.scope;
+  }
+}
